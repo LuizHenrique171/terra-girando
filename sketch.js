3edd1369d2a606c207cb00a14b46b67084e8ba5e
LuizHenrique171 <luiz.pulcinelli.cruz@escola.pr.gov.br>
2025-08-05 09:56:52 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,25 @@
-let terra;
-let texturaEstrelas;
+unction setup() {
 
-function preload() {
-  terra = loadImage('terra.jpg');
-  // Você precisará de uma imagem de estrelas para isso
-  texturaEstrelas = loadImage('estrelas.jpg'); 
+  createCanvas(1920, 1080, WEBGL); //plano 3D
+
+  terra=loadImage('terra.jpg')
+
 }
 
-function setup() {
-  createCanvas(1920, 1080, WEBGL);
-}
+
 
 function draw() {
-  background(0); // Fundo preto para o espaço
 
-  // Adicione estrelas como um fundo usando uma textura em uma esfera grande
-  push();
-  noStroke(); // Remove o contorno
-  texture(texturaEstrelas);
-  // Uma esfera bem grande que age como o "céu"
-  sphere(2500); 
-  pop();
+  background(0, 0, 0); //cor de fundo
 
-  // Desenha a Terra girando
-  push();
-  // Uma luz ambiente para que o lado escuro não fique totalmente preto
-  ambientLight(50); 
-  let locX = mouseX - width / 2;
-  let locY = mouseY - height / 2;
-  // Uma fonte de luz que segue o mouse
-  pointLight(255, 255, 255, locX, locY, 100); 
-  
-  rotateY(millis() / 2000);
-  texture(terra);
-  sphere(280);
-  pop();
+  rotateY(millis()/2000); // rotação da terra
+
+  texture(terra); //textura da terra, imagem.
+
+  sphere(280, 300); //esfera 3D
+
+  
+
+  
+
 }
